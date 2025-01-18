11f1daf8b548a72e30204c07910b64bb696dcf00
garvjainus <garv.jainn@gmail.com>
2025-01-18 17:43:00 -0500

    new shit

--- Sliding Window/LongestRepeatingCharacterReplacement.java
+++ Sliding Window/LongestRepeatingCharacterReplacement.java
@@ -8,19 +8,20 @@ class Solution {
             char val1 = s.charAt(right);
             char val2 = s.charAt(left);
             map.put(val1, map.getOrDefault(val1, 0) + 1);
-            char max = '\0';
             int maxVal = Integer.MIN_VALUE;
             for(char c: map.keySet()){
+                //update maxVal to the character that appears most in the given window
                 if(map.get(c) > maxVal){
-                    max = c;
                     maxVal = map.get(c);
                 }
             }
             int winLength = right - left + 1;
+            //check if window length - the most occurent character has enough replacements to create a contiguous substring
             if(winLength - maxVal > k){
                 map.put(val2, map.get(val2) - 1);
                 left++;
             }
+            //not winLength because we had to recalculate with new left value
             output = Math.max(output, right - left + 1);
        }
        return output;
