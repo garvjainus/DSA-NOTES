fa82ae84786768035aa1624b7630a5fa85d56caf
Garv Jain <garv.jainn@gmail.com>
2025-01-26 14:30:05 -0500

    blah

--- arrays/JumpGame.java
+++ arrays/JumpGame.java
@@ -12,9 +12,9 @@ class Solution {
         for(int i = 1; i < nums.length; i++){
             maxJumps = Math.max(maxJumps, nums[i]); //this allows us to ignore nums[i], because it gives us the amount of jumps we can use at any given point, if we have 3 and then 2 as the next value in the array, 3 steps will become 2 through decrement and still apply
             //only time you need to update the amount of jumps you can do at a given point is if the number of jumps at index i is greater than the maxjumps recorded before, as long as we keep decrementing maxjumps to keep track of how many jumps we can possibly do
-            if(nums[i] == 0 && maxJumps == 0){
-                break;
-            }
+                if(nums[i] == 0 && maxJumps == 0){
+                    break;
+                }
             if(i + maxJumps >= target){
                 return true;
             }
